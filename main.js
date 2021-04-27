c8c801061d841494ba17deca19ab517607b2f062
Zaitian Mou <zaitian.mou@gmail.com>
2021-04-26 23:51:17 -0400

    Convert input to desired representation. Problem done.

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
-input=[]
-
 //read input from command line argument
+input=[]
 process.argv.forEach(function (val, index, array) {
   input.push(val)
 });
-input.splice(0,2)
 
-//parse them into integers
-nums=[]
-input.forEach(function(val){
-	nums.push(parseInt(val, 10))
+//remove the first args, that is "node" and "main.js"
+input.splice(0,2) 
+
+//converte each element into string representation
+output_list=[]
+input.forEach(function(s){
+	var res=""
+	for (var i=0; i<s.length; i++){
+		if (s.charAt(i)==="0")
+			res=res.concat("Zero")
+		else if (s.charAt(i)==="1")
+			res=res.concat("One")
+		else if (s.charAt(i)==="2")
+			res=res.concat("Two")
+		else if (s.charAt(i)==="3")
+			res=res.concat("Three")
+		else if (s.charAt(i)==="4")
+			res=res.concat("Four")
+		else if (s.charAt(i)==="5")
+			res=res.concat("Five")
+		else if (s.charAt(i)==="6")
+			res=res.concat("Six")
+		else if (s.charAt(i)==="7")
+			res=res.concat("Seven")
+		else if (s.charAt(i)==="8")
+			res=res.concat("Eight")
+		else if (s.charAt(i)==="9")
+			res=res.concat("Nine")
+	}
+	output_list.push(res)
 })
 
-
-
-console.log(nums)
+//Finally, concatenate all the strings and print it.
+console.log(output_list.join())
